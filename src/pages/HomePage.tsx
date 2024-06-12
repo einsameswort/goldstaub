ec22fd64c44f71025cb7bd250db2e9af532c3056
einsameswort <einsameswort@proton.me>
2024-06-12 23:19:03 +0200

    Styling the crafting grid

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,5 +5,33 @@ import { DefaultLayout } from '@layouts/DefaultLayout';
 export const HomePage: React.FC = () => (
   <DefaultLayout>
     <h1>Home</h1>
+    <div className="crafting">
+      <div className="crafting__items">
+        <div className="crafting__item">
+          <div className="crafting__item__icon crafting__item__icon--red"></div>
+          <p className="crafting__item__name">Item 1</p>
+        </div>
+        <div className="crafting__item">
+          <div className="crafting__item__icon crafting__item__icon--green"></div>
+          <p className="crafting__item__name">Item 2</p>
+        </div>
+        <div className="crafting__item">
+          <div className="crafting__item__icon crafting__item__icon--blue"></div>
+          <p className="crafting__item__name">Item 3</p>
+        </div>
+      </div>
+      <div className="crafting__grid">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className="crafting__result"></div>
+    </div>
   </DefaultLayout>
 );
